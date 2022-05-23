ae277b6fa022be57e0d4a80642d52a87e55b5c9d
Juan David Mendez Campuzano <juandavidmendezcampuzano@gmail.com>
2022-05-23 16:57:30 -0500

    Proyecto listo para produccion

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ app.use('/api/auth',require('./routes/auth.route'));
 // CRUD: eventos
 app.use('/api/events',require('./routes/events.route'));
 
+//app.use( '*', express.static('public') );
+
 //Escuchar peticiones
 app.listen( process.env.PORT,()=>{
     console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
